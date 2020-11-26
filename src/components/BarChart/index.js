29c8d7bf48357ba4fa9e1a62e8a98277fe1da339
Greg Sandell <gsandell_99@yahoo.com>
2020-11-26 13:00:21 -0800

    Extract code from <Home> into new <BarChart>

---
+++ src/components/BarChart/index.js
@@ -0,0 +1,63 @@
+import React, { Fragment } from 'react'
+import ReactTooltip from 'react-tooltip'
+import Util from 'lib/Util'
+
+const BAR_COLORS = [{ bg: '#003f5c', fg: 'white' }, { bg: '#bc5090', fg: 'white' }, { bg: '#ffa600', fg: 'black' }]
+const HORIZONTAL_CHAR_WIDTH = 8
+
+// eslint-disable-next-line react/prop-types
+const BarChart = ({ filteredData, windowWidth, newMax, skinnyBars, barHeight }) => {
+  return (
+    <Fragment>
+      {
+        // eslint-disable-next-line react/prop-types
+        filteredData.map((item, i) => {
+          // console.log(`(${item.value}/${max}) * ${this.state.windowWidth} = ${(item.value / max) * this.state.windowWidth}`)
+          const size = item.value
+          const barWidth = (size / newMax) * windowWidth
+          const style = {
+            width: barWidth,
+            height: barHeight + 'px',
+            backgroundColor: BAR_COLORS[i % 3].bg,
+            color: BAR_COLORS[i % 3].fg
+          }
+          const text = `${item.Country_Code} - ${item.Country} - ${Util.millionize(size)} mil`
+          const textPixels = text.length * HORIZONTAL_CHAR_WIDTH
+          let bar
+          switch (true) {
+          case skinnyBars:
+            bar = (
+              <div key={'c' + i}>
+                <div className='bar' style={style} alt={text} data-tip data-for={`bar${i}`} />
+                <ReactTooltip id={`bar${i}`} place='bottom' type='dark' effect='solid'>
+                  <span>{text}</span>
+                </ReactTooltip>
+              </div>
+            )
+            break
+          case barWidth > textPixels:
+            bar = (
+              <div
+                className='bar' style={style} key={'c' + i} title={text}>
+                <div className='name-internal'>{text}</div>
+              </div>
+            )
+            break
+          case barWidth <= textPixels:
+          default:
+            bar = (
+              <div key={'c' + i}>
+                <div
+                  className='bar shortbar' style={style} title={text} />
+                <div className='name-external'>{text}</div>
+              </div>
+            )
+          }
+          return bar
+        })
+      }
+    </Fragment>
+  )
+}
+
+export default BarChart
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,14 @@
 import React, { useEffect, useState } from 'react'
-import ReactTooltip from 'react-tooltip'
 // import Tooltip from '@material-ui/core/Tooltip'
 import CountryDAO from 'lib/CountryDAO'
 import Util from 'lib/Util'
 import YearSelector from 'components/YearSelector'
 import BonusControls from 'components/BonusControls'
+import BarChart from 'components/BarChart'
 import './home.css'
 
 const RESIZE_DELAY = 250
 const CHART_MARGIN = 20
-const BAR_COLORS = [{ bg: '#003f5c', fg: 'white' }, { bg: '#bc5090', fg: 'white' }, { bg: '#ffa600', fg: 'black' }]
-const HORIZONTAL_CHAR_WIDTH = 8
 const WIDE_BARS = 20
 
 const debounce = Util.debounceFactory()
@@ -89,52 +87,9 @@ const Home = () => {
           setBarHeight={setBarHeight} />
         <div className='card'>
           <div className='card-body'>
-            {
-              filteredData.map((item, i) => {
-                // console.log(`(${item.value}/${max}) * ${this.state.windowWidth} = ${(item.value / max) * this.state.windowWidth}`)
-                const size = item.value
-                const barWidth = (size / newMax) * windowWidth
-                const style = {
-                  width: barWidth,
-                  height: barHeight + 'px',
-                  backgroundColor: BAR_COLORS[i % 3].bg,
-                  color: BAR_COLORS[i % 3].fg
-                }
-                const text = `${item.Country_Code} - ${item.Country} - ${Util.millionize(size)} mil`
-                const textPixels = text.length * HORIZONTAL_CHAR_WIDTH
-                let bar
-                switch (true) {
-                case skinnyBars:
-                  bar = (
-                    <div key={'c' + i}>
-                      <div className='bar' style={style} alt={text} data-tip data-for={`bar${i}`} />
-                      <ReactTooltip id={`bar${i}`} place='bottom' type='dark' effect='solid'>
-                        <span>{text}</span>
-                      </ReactTooltip>
-                    </div>
-                  )
-                  break
-                case barWidth > textPixels:
-                  bar = (
-                    <div
-                      className='bar' style={style} key={'c' + i} title={text}>
-                      <div className='name-internal'>{text}</div>
-                    </div>
-                  )
-                  break
-                case barWidth <= textPixels:
-                default:
-                  bar = (
-                    <div key={'c' + i}>
-                      <div
-                        className='bar shortbar' style={style} title={text} />
-                      <div className='name-external'>{text}</div>
-                    </div>
-                  )
-                }
-                return bar
-              })
-            }
+            <BarChart
+              filteredData={filteredData} windowWidth={windowWidth} newMax={newMax}
+              skinnyBars={skinnyBars} barHeight={barHeight} />
           </div>
         </div>
       </div>)
